ef500ebe1034721900ad038784c9bc88632ddab2
jongmun <sorisum@gmail.com>
2019-08-13 10:16:54 +0900

    라이선스 표기 추가

--- src/tizen2thingspark.c
+++ src/tizen2thingspark.c
@@ -1,3 +1,28 @@
+/*****************************************************************
+ *
+ * Copyright (c) 2019 theksystem Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************/
+
+/***********************************************************************
+ Filename:   tizen2thingspark.c
+ Author: theksystem
+ ************************************************************************/
+
+
+
 #include <tizen.h>
 #include <service_app.h>
 #include <Ecore.h>
@@ -13,6 +38,7 @@ Eina_Bool _get_sensor_value(void *data) {
 	tp_handle_h handle = NULL;
 
 	// TODO API_KEY 변경
+	// thingspark.kr 에서 채널별로 부여되는 api-key를 입력합니다.
 	ret = tp_initialize("API_KEY", &handle);
 	retv_if(ret != 0, -1);
 
