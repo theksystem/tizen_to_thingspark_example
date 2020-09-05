e4fa8876a1776e0152a4041e55637832731c6da4
jongmoon <jm.kim@theksystem.com>
2020-09-05 17:30:57 +0900

    -

--- src/tizen_2_thingspark_example.c
+++ src/tizen_2_thingspark_example.c
@@ -81,83 +81,6 @@ Eina_Bool _get_sensor_value(void *data) {
 }
 
 
-
-/* callback for curl fetch */
-size_t curl_callback (void *contents, size_t size, size_t nmemb, void *userp) {
-    size_t realsize = size * nmemb;                             /* calculate buffer size */
-    struct curl_fetch_st *p = (struct curl_fetch_st *) userp;   /* cast pointer to fetch struct */
-
-    /* expand buffer */
-    p->payload = (char *) realloc(p->payload, p->size + realsize + 1);
-
-    /* check buffer */
-    if (p->payload == NULL) {
-      /* this isn't good */
-      fprintf(stderr, "ERROR: Failed to expand buffer in curl_callback");
-      /* free buffer */
-      free(p->payload);
-      /* return */
-      return 1;
-    }
-
-    /* copy contents to buffer */
-    memcpy(&(p->payload[p->size]), contents, realsize);
-
-    /* set new buffer size */
-    p->size += realsize;
-
-    /* ensure null termination */
-    p->payload[p->size] = 0;
-
-    /* return size */
-    return realsize;
-}
-
-CURLcode curl_fetch_url(CURL *ch, const char *url, struct curl_fetch_st *fetch) {
-    CURLcode rcode;                   /* curl result code */
-
-    /* init payload */
-    fetch->payload = (char *) calloc(1, sizeof(fetch->payload));
-
-    /* check payload */
-    if (fetch->payload == NULL) {
-        /* log error */
-        fprintf(stderr, "ERROR: Failed to allocate payload in curl_fetch_url");
-        /* return error */
-        return CURLE_FAILED_INIT;
-    }
-
-    /* init size */
-    fetch->size = 0;
-
-    /* set url to fetch */
-    curl_easy_setopt(ch, CURLOPT_URL, url);
-
-    /* set calback function */
-    curl_easy_setopt(ch, CURLOPT_WRITEFUNCTION, curl_callback);
-
-    /* pass fetch struct pointer */
-    curl_easy_setopt(ch, CURLOPT_WRITEDATA, (void *) fetch);
-
-    /* set default user agent */
-    curl_easy_setopt(ch, CURLOPT_USERAGENT, "libcurl-agent/1.0");
-
-    /* set timeout */
-    curl_easy_setopt(ch, CURLOPT_TIMEOUT, 15);
-
-    /* enable location redirects */
-    curl_easy_setopt(ch, CURLOPT_FOLLOWLOCATION, 1);
-
-    /* set maximum allowed redirects */
-    curl_easy_setopt(ch, CURLOPT_MAXREDIRS, 1);
-
-    /* fetch the url */
-    rcode = curl_easy_perform(ch);
-
-    /* return */
-    return rcode;
-}
-
 Eina_Bool _get_thingspark_last_channel_entry(void *data) {
 
 	CURL *ch;                                               /* curl handle */
@@ -250,6 +173,84 @@ Eina_Bool _get_thingspark_last_channel_entry(void *data) {
 	return ECORE_CALLBACK_RENEW;
 }
 
+
+
+
+/* callback for curl fetch */
+size_t curl_callback (void *contents, size_t size, size_t nmemb, void *userp) {
+    size_t realsize = size * nmemb;                             /* calculate buffer size */
+    struct curl_fetch_st *p = (struct curl_fetch_st *) userp;   /* cast pointer to fetch struct */
+
+    /* expand buffer */
+    p->payload = (char *) realloc(p->payload, p->size + realsize + 1);
+
+    /* check buffer */
+    if (p->payload == NULL) {
+      /* this isn't good */
+      fprintf(stderr, "ERROR: Failed to expand buffer in curl_callback");
+      /* free buffer */
+      free(p->payload);
+      /* return */
+      return 1;
+    }
+
+    /* copy contents to buffer */
+    memcpy(&(p->payload[p->size]), contents, realsize);
+
+    /* set new buffer size */
+    p->size += realsize;
+
+    /* ensure null termination */
+    p->payload[p->size] = 0;
+
+    /* return size */
+    return realsize;
+}
+
+CURLcode curl_fetch_url(CURL *ch, const char *url, struct curl_fetch_st *fetch) {
+    CURLcode rcode;                   /* curl result code */
+
+    /* init payload */
+    fetch->payload = (char *) calloc(1, sizeof(fetch->payload));
+
+    /* check payload */
+    if (fetch->payload == NULL) {
+        /* log error */
+        fprintf(stderr, "ERROR: Failed to allocate payload in curl_fetch_url");
+        /* return error */
+        return CURLE_FAILED_INIT;
+    }
+
+    /* init size */
+    fetch->size = 0;
+
+    /* set url to fetch */
+    curl_easy_setopt(ch, CURLOPT_URL, url);
+
+    /* set calback function */
+    curl_easy_setopt(ch, CURLOPT_WRITEFUNCTION, curl_callback);
+
+    /* pass fetch struct pointer */
+    curl_easy_setopt(ch, CURLOPT_WRITEDATA, (void *) fetch);
+
+    /* set default user agent */
+    curl_easy_setopt(ch, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+    /* set timeout */
+    curl_easy_setopt(ch, CURLOPT_TIMEOUT, 15);
+
+    /* enable location redirects */
+    curl_easy_setopt(ch, CURLOPT_FOLLOWLOCATION, 1);
+
+    /* set maximum allowed redirects */
+    curl_easy_setopt(ch, CURLOPT_MAXREDIRS, 1);
+
+    /* fetch the url */
+    rcode = curl_easy_perform(ch);
+
+    /* return */
+    return rcode;
+}
 bool service_app_create(void *data) {
 	// Todo: add your code here.
 
